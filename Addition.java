79bf906172f3b44e95444c2b4af628f9420c766d
Heved Ridel <exxed.ridel@gmail.com>
2023-01-10 21:35:46 -0600

    Add int values syntax

---
+++ Addition.java
@@ -0,0 +1,10 @@
+public class Addition{
+	public static void main(String args []){
+		int num_one = 5;
+		int num_two = 2;
+		int result = 0;
+		
+		result = num_one + num_two;
+		System.out.println("The addition value is: " + result);
+	}
+}
